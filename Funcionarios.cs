eabf786df6a02dab4948c3d8f8e51b163a1d9216
Yasmin Vieira <yasminvpinheiro19@gmail.com>
2024-10-12 23:12:31 -0300

    Create Funcionarios.cs

---
+++ Funcionarios.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1_Cell.Modelo
+{
+  public class Funcionario
+  {
+    public string Nome { get; set; }
+    public string Cargo { get; set; }
+    public string Foto { get; set; }
+  }
+}
